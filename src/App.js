081bba136a2116b94dd309138731a7fbd3bee55b
Jack <jack.dang@it-artists.ch>
2024-04-29 10:33:06 +0700

    Final Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <div>Homepage</div>
-    </div>
+    <>
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </>
   );
 }
 
